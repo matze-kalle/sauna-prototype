1df60f1165fe925e9bade0df99f06d20e24e95f1
Luca Feigl <sarius009@gmail.com>
2019-08-04 01:24:28 +0200

    Add vestigial hot reload logic

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+const path = require('path');
+const chokidar = require('chokidar');
 const express = require('express');
+const watch = path.resolve('./api/');
+const watcher = chokidar.watch(watch);
 const server = express();
 
 server.get('/', (req, res, next) => {
   res.send('Sauna Prototype');
+});
+
+server.use((req, res, next) => {
+  require('./api/about.js')(req, res, next);
+})
+
+watcher.on('ready', () => {
+  watcher.on('all', (event, path) => {
+    console.log(event, path);
+    Object.keys(require.cache).forEach((id) => {
+      if (/[/\\]api[/\\]/.test(id)) {
+        delete require.cache[id];
+      }
+    });
+  });
 });
 
 server.listen(3000);
